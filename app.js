972639788ee630f81ed38f22be82910b9ae2c52f
Ari Robbins <ari.robbins@gmail.com>
2016-12-14 08:59:43 -0500

    container design nearly complete\!

--- app.js
+++ app.js
@@ -2,13 +2,49 @@
 var state = {
     questionSets: [
         {
-            id: 1,
+            id: 0,
             question: "Which of these is not one of the seven dwarves?",
             a: "Happy",
             b: "Dippy",
             c: "Grumpy",
             d: "Dopey",
             answer: "b"  
+        },
+        {
+            id: 1,
+            question: "In the story of Jack and the Beanstalk, which of these items is not an item that Jack stole from the giant?",
+            a: "A bag of gold",
+            b: "A harp",
+            c: "A sword",
+            d: "The goose",
+            answer: "c"  
+        },
+        {
+            id: 2,
+            question: "Which of these fairy tale characters loses a shoe?",
+            a: "Red Rose",
+            b: "Belle",
+            c: "Snow White",
+            d: "Cinderella",
+            answer: "d"  
+        },
+        {
+            id: 3,
+            question: "In which popular story would you find a talking cricket?",
+            a: "Peter Pan",
+            b: "Pinocchio",
+            c: "Cinderella",
+            d: "Briar Rose",
+            answer: "b"  
+        },
+        {
+            id: 4,
+            question: "The character of Maleficent appears in which fairy tale?",
+            a: "Snow White",
+            b: "Thumbelina",
+            c: "Red Rose",
+            d: "Sleeping Beauty",
+            answer: "d"  
         }
     ]
 };
